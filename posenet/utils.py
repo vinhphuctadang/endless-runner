d9ce5667b2a6a2d1182dc1f53b5f1046eb80dcc6
Tinh Dao <dcongtinh@gmail.com>
2021-03-03 20:18:12 +0700

    update read frame from webcam&video

--- posenet/utils.py
+++ posenet/utils.py
@@ -11,23 +11,27 @@ def valid_resolution(width, height, output_stride=16):
 
 
 def _process_input(source_img, scale_factor=1.0, output_stride=16):
+
     target_width, target_height = valid_resolution(
         source_img.shape[1] * scale_factor, source_img.shape[0] * scale_factor, output_stride=output_stride)
-    scale = np.array([source_img.shape[0] / target_height, source_img.shape[1] / target_width])
+    scale = np.array([source_img.shape[0] / target_height,
+                      source_img.shape[1] / target_width])
 
-    input_img = cv2.resize(source_img, (target_width, target_height), interpolation=cv2.INTER_LINEAR)
+    input_img = cv2.resize(
+        source_img, (target_width, target_height), interpolation=cv2.INTER_LINEAR)
     input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2RGB).astype(np.float32)
 
     input_img = input_img * (2.0 / 255.0) - 1.0
-    input_img = cv2.flip(input_img, 1) # Flip
-    source_img = cv2.flip(source_img, 1) # Flip
     input_img = input_img.reshape(1, target_height, target_width, 3)
 
     return input_img, source_img, scale
 
 
-def read_cap(cap, scale_factor=1.0, output_stride=16):
+def read_cap(cap, flip=False, scale_factor=1.0, output_stride=16):
     res, img = cap.read()
+    img = cv2.resize(img, (375, 480))
+    if flip:
+        img = cv2.flip(img, 1)  # Flip horizontal
     if not res:
         raise IOError("webcam failure")
     return _process_input(img, scale_factor, output_stride)
@@ -59,7 +63,8 @@ def get_adjacent_keypoints(keypoint_scores, keypoint_coords, min_confidence=0.1)
         if keypoint_scores[left] < min_confidence or keypoint_scores[right] < min_confidence:
             continue
         results.append(
-            np.array([keypoint_coords[left][::-1], keypoint_coords[right][::-1]]).astype(np.int32),
+            np.array([keypoint_coords[left][::-1],
+                      keypoint_coords[right][::-1]]).astype(np.int32),
         )
     return results
 
@@ -75,7 +80,8 @@ def draw_skeleton(
         new_keypoints = get_adjacent_keypoints(
             keypoint_scores[ii, :], keypoint_coords[ii, :, :], min_part_confidence)
         adjacent_keypoints.extend(new_keypoints)
-    out_img = cv2.polylines(out_img, adjacent_keypoints, isClosed=False, color=(255, 255, 0))
+    out_img = cv2.polylines(out_img, adjacent_keypoints,
+                            isClosed=False, color=(255, 255, 0))
     return out_img
 
 
@@ -101,5 +107,14 @@ def draw_skel_and_kp(
     out_img = cv2.drawKeypoints(
         out_img, cv_keypoints, outImage=np.array([]), color=(255, 255, 0),
         flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    out_img = cv2.polylines(out_img, adjacent_keypoints, isClosed=False, color=(255, 255, 0))
+    out_img = cv2.polylines(out_img, adjacent_keypoints,
+                            isClosed=False, color=(255, 255, 0))
+    return out_img
+
+
+def draw_fps(img, fps):
+    fps = str(fps)
+    out_img = img
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(out_img, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
     return out_img
--- webcam_demo.py
+++ webcam_demo.py
@@ -11,7 +11,7 @@ parser.add_argument('--model', type=int, default=101)
 parser.add_argument('--cam_id', type=int, default=0)
 parser.add_argument('--cam_width', type=int, default=257)  # 1280
 parser.add_argument('--cam_height', type=int, default=257)  # 720
-parser.add_argument('--scale_factor', type=float, default=0.7125)
+parser.add_argument('--scale_factor', type=float, default=0.5)
 parser.add_argument('--file', type=str, default=None,
                     help="Optionally use a video file instead of a live camera")
 args = parser.parse_args()
@@ -24,8 +24,10 @@ def main():
 
         if args.file is not None:
             cap = cv2.VideoCapture(args.file)
+            flip = False
         else:
             cap = cv2.VideoCapture(args.cam_id)
+            flip = True
         cap.set(3, args.cam_width)
         cap.set(4, args.cam_height)
 
@@ -38,9 +40,29 @@ def main():
         #     do = csv.writer(w)
         #     do.writerow(header)
 
+        # used to record the time when we processed last frame
+        prev_frame_time = 0
+
+        # used to record the time at which we processed current frame
+        new_frame_time = 0
+
         while True:
             input_image, display_image, output_scale = posenet.read_cap(
-                cap, scale_factor=args.scale_factor, output_stride=output_stride)
+                cap, flip=flip, scale_factor=args.scale_factor, output_stride=output_stride)
+
+            # time when we finish processing for this frame
+            new_frame_time = time.time()
+
+            # Calculating the fps
+
+            # fps will be number of frame processed in given time frame
+            # since their will be most of time error of 0.001 second
+            # we will be subtracting it to get more accurate result
+            fps = 1 / (new_frame_time - prev_frame_time)
+            prev_frame_time = new_frame_time
+
+            # converting the fps into integer
+            fps = int(fps)
 
             heatmaps_result, offsets_result, displacement_fwd_result, displacement_bwd_result = sess.run(
                 model_outputs,
@@ -69,17 +91,17 @@ def main():
                 position.append(i + 1)
                 x_coordinates.append(j[0])
                 y_coordinates.append(j[1])
-            print(
-                f'Frame is {frame}, body pos {position}, x value {x_coordinates}, y value {y_coordinates}')
+            # print(
+            #     f'Frame is {frame}, body pos {position}, x value {x_coordinates}, y value {y_coordinates}')
+
+            display_image = posenet.draw_fps(display_image, fps)
 
             # TODO this isn't particularly fast, use GL for drawing and display someday...
             overlay_image = posenet.draw_skel_and_kp(
                 display_image, pose_scores, keypoint_scores, keypoint_coords,
                 min_pose_score=0.15, min_part_score=0.1)
-            # print(cap.get(cv2.CAP_PROP_FPS))
-            # print(len(keypoint_scores[0]))
 
-            print(keypoint_coords[0])
+            # print(keypoint_coords[0])
 
             cv2.imshow('posenet', overlay_image)
             frame_count += 1
@@ -87,8 +109,8 @@ def main():
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-            print('Average FPS: ', frame_count / (time.time() - start))
-
+            # print('Average FPS: ', frame_count / (time.time() - start))
+            # print('Average FPS: ', fps)
             rows = zip(action_name, frame, position,
                        x_coordinates, y_coordinates)
 
